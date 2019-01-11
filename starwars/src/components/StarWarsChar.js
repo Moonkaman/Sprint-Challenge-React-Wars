78521952fdbeaa2da6c76562656d6b86c7135ff9
Moonkaman <nickbatmanglidj@gmail.com>
2019-01-11 10:09:47 -0700

    Tweak base styles

--- starwars/src/components/StarWarsChar.js
+++ starwars/src/components/StarWarsChar.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.css'
+
 const StarWarsChar = props => {
   return (
     <div className="starwars-char-cont">
+      <i className="fas fa-user starwars-char-icon"></i>
       <h2 className="starwars-char-name">{props.charName}</h2>
       <h3 className="starwars-char-born">Born: {props.charBirth}</h3>
     </div>
